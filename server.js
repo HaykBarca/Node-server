6cf9bdc3ea1632c3b203c64bb232a8fde2403c63
HaykBarca <hayk19939563@gmail.com>
2018-11-24 13:48:09 +0400

    New Features

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-    res.render('projects.hbs');
+    res.render('projects.hbs', {
+        pageName: 'Projects'
+    });
 });
 
 app.get('/about', (req, res) => {
